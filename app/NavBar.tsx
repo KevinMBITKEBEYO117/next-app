d08355aae504ed647ec24fe08d39d7fc6ea28184
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-12-14 08:44:48 -0500

    implementing next auth and setting up react-email

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
-  return (
-    <div className='flex bg-slate-200 text-black'>
-        <Link href='/' className='mr-5'>Next.js</Link>
-        <Link href='/users' className='mr-5'>Users</Link>
-    </div>
-  )
-}
+  const { status, data: session } = useSession();
 
-export default NavBar
+  return (
+    <div className="flex bg-slate-200 text-black">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users" className="mr-5">
+        Users
+      </Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div> {session.user!.name} <Link href="/api/auth/signout">Sign out</Link> </div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-'use client'
-
 import Image from 'next/image'
 import Link from 'next/link'
 import ProductsCard from './components/ProductsCard'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+ const session = await getServerSession(authOptions)
   return (
    <main>
     
-    <h1>Hello world</h1>
+    <h1 className='text-black'> Hello {session?.user?.name} </h1>
     <Link href="/users">Users</Link>
     <ProductsCard/>
     </main>
